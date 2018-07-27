8497c68f5184cf0b3d1c61941b1f9d5fa6f51f16
Jonathan Spratley <jonniespratley@gmail.com>
2018-07-27 14:18:17 -0700

    added material ui styles

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,33 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
+require('./lib/pace.min.js')
+Pace.restart()
+
+
 $(() => {
+
+ //   pace.start()
+  // var toolbar = mdc.toolbar.MDCToolbar.attachTo(document.querySelector('.mdc-toolbar'));
+  // toolbar.fixedAdjustElement = document.querySelector('.mdc-toolbar-fixed-adjust');
+  /*
+   toolbar.listen('MDCToolbar:change', function(evt) {
+    var flexibleExpansionRatio = evt.detail.flexibleExpansionRatio;
+    console.log(flexibleExpansionRatio.toFixed(2));
+  });
+   */
+
+  let drawerOpen = false;
+  const app = document.querySelector('.app');
+  //const drawer = new mdc.drawer.MDCPersistentDrawer(document.querySelector('.mdc-drawer--persistent'));
+  const drawer = new mdc.drawer.MDCTemporaryDrawer(document.querySelector(".mdc-drawer--temporary"));
+  const drawerToggle = document.querySelector('.layout-drawer__toggle')
+
+  drawerToggle.addEventListener("click", function () {
+    drawer.open = !drawer.open;
+    drawerOpen = drawer.open;
+    //app.classList.toggle('drawer-open');
+  });
+
     console.log('application.js loaded');
 });
+
--- webpack.config.js
+++ webpack.config.js
@@ -9,89 +9,127 @@ const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 const configurator = {
-  entries: function(){
+  entries: function () {
     var entries = {
-      application: [
-        './node_modules/jquery-ujs/src/rails.js',
-        './assets/css/application.scss',
-      ],
+      application: ['./node_modules/jquery-ujs/src/rails.js', './assets/css/application.scss']
     }
-    
-    Glob.sync("./assets/*/*.*").forEach((entry) => {
-      if (entry === './assets/css/application.scss') {
-        return
-      }
-      
-      let key = entry.replace(/(\.\/assets\/(js|css|go)\/)|\.(js|s[ac]ss|go)/g, '')
-      if(key.startsWith("_") || (/(js|s[ac]ss|go)$/i).test(entry) == false) {
-        return
-      }
-      
-      if( entries[key] == null) {
-        entries[key] = [entry]
-        return
-      } 
-      
-      entries[key].push(entry)
-    })
+
+    Glob
+      .sync("./assets/*/*.*")
+      .forEach((entry) => {
+        if (entry === './assets/css/application.scss') {
+          return
+        }
+
+        let key = entry.replace(/(\.\/assets\/(js|css|go)\/)|\.(js|s[ac]ss|go)/g, '')
+        if (key.startsWith("_") || (/(js|s[ac]ss|go)$/i).test(entry) == false) {
+          return
+        }
+
+        if (entries[key] == null) {
+          entries[key] = [entry]
+          return
+        }
+
+        entries[key].push(entry)
+      })
 
     return entries
   },
 
   plugins() {
     var plugins = [
-      new CleanObsoleteChunks(),
-      new Webpack.ProvidePlugin({$: "jquery",jQuery: "jquery"}),
+      new CleanObsoleteChunks(), new Webpack.ProvidePlugin({$: "jquery", jQuery: "jquery"}),
       new MiniCssExtractPlugin({filename: "[name].[contenthash].css"}),
-      new CopyWebpackPlugin([{from: "./assets",to: ""}], {copyUnmodified: true,ignore: ["css/**", "js/**"] }),
-      new Webpack.LoaderOptionsPlugin({minimize: true,debug: false}),
+      new CopyWebpackPlugin([
+        {
+          from: "./assets",
+          to: ""
+        }
+      ], {
+        copyUnmodified: true,
+        ignore: ["css/**", "js/**"]
+      }),
+      new Webpack.LoaderOptionsPlugin({minimize: true, debug: false}),
       new ManifestPlugin({fileName: "manifest.json"})
     ];
 
     return plugins
   },
 
-  moduleOptions: function() {
+  moduleOptions: function () {
     return {
       rules: [
         {
           test: /\.s[ac]ss$/,
           use: [
-            MiniCssExtractPlugin.loader,
-            { loader: "css-loader", options: {sourceMap: true}},
-            { loader: "sass-loader", options: {sourceMap: true}}
+            MiniCssExtractPlugin.loader, {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            }, {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
           ]
-        },
-        { test: /\.jsx?$/,loader: "babel-loader",exclude: /node_modules/ },
-        { test: /\.(woff|woff2|ttf|svg)(\?v=\d+\.\d+\.\d+)?$/,use: "url-loader"},
-        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,use: "file-loader" },
-        { test: require.resolve("jquery"),use: "expose-loader?jQuery!expose-loader?$"},
-        { test: /\.go$/, use: "gopherjs-loader"}
+        }, {
+          test: /\.js?$/,
+          loader: "babel-loader",
+          //exclude: /node_modules/
+        }, {
+          test: /\.(woff|woff2|ttf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: "url-loader"
+        }, {
+          test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+          use: "file-loader"
+        }, {
+          test: require.resolve("jquery"),
+          use: "expose-loader?jQuery!expose-loader?$"
+        }, {
+          test: /\.go$/,
+          use: "gopherjs-loader"
+        }
       ]
     }
   },
 
-  buildConfig: function(){
+  buildConfig: function () {
     const env = process.env.NODE_ENV || "development";
-    
+
     var config = {
       mode: env,
       entry: configurator.entries(),
-      output: {filename: "[name].[hash].js", path: `${__dirname}/public/assets`},
+      output: {
+        filename: "[name].[hash].js",
+        path: `${__dirname}/public/assets`
+      },
       plugins: configurator.plugins(),
-      module: configurator.moduleOptions()
+      module: configurator.moduleOptions(),
+      resolve: {
+        modules: [path.resolve(__dirname, "assets"), "node_modules"],
+        extensions: [".js", ".json", ".scss", ".css"]
+      }
     }
 
-    if( env === "development" ){
-      config.plugins.push(new LiveReloadPlugin({appendScriptTag: true}))
+    if (env === "development") {
+      config
+        .plugins
+        .push(new LiveReloadPlugin({appendScriptTag: true}))
       return config
     }
 
     const uglifier = new UglifyJsPlugin({
       uglifyOptions: {
         beautify: false,
-        mangle: {keep_fnames: true},
-        output: {comments: false},
+        mangle: {
+          keep_fnames: true
+        },
+        output: {
+          comments: false
+        },
         compress: {}
       }
     })
